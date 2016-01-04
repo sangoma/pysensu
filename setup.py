a29619fa1b0bac5d226aec7a514ae2b3799429ef
Angel Velásquez <angvp@archlinux.org>
2016-01-04 20:04:22 -0300

    First release on PyPI

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='pysensu-ng',
-    version='0.1.0',
+    version='0.2.0',
     description='This is a client for interact with the Sensu API',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Angel Velasquez',
